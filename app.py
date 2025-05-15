9682220704dc2769a5eb71373fa5633b0593c6d0
alex <alexfraleda@gmail.com>
2025-05-15 16:48:23 +0200

    Comit

--- app.py
+++ app.py
@@ -1,55 +1,64 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
-import datetime
-from datetime import date
+from datetime import date, datetime
+from bson.objectid import ObjectId
 
 class Usuario:
-    def __init__(self, nombre, email, contraseña, fecha_registro):
+    def __init__(self, nombre, email, contraseña, fecha_registro ,activo , numero_pedidos):
         self.nombre = nombre
         self.email = email
         self.contraseña = contraseña
         self.fecha_registro = fecha_registro
+        self.activo = activo
+        self.numero_pedidos = numero_pedidos
 
     def to_dict(self):
         return {
             "nombre": self.nombre,
             "email": self.email,
             "contraseña": self.contraseña,
-            "fecha_registro": self.fecha_registro
+            "fecha_registro": self.fecha_registro,
+            "activo" : self.activo ,
+            "numero_pedidos" : self.numero_pedidos
         }
 
 
 
 app = Flask(__name__)
 
-online = MongoClient("mongodb+srv://jjen527:35OpRHzih8l1N14m@jjannik.aliwych.mongodb.net/")
+online = MongoClient("mongodb+srv://arodsan013a:T1L7WBu0UDrxvMtH@alexz.lqhssf7.mongodb.net/")
 
 app.db = online.tienda
 
-nombre_admin = "Jannik"
+nombre_admin = "Alejandro"
 tienda = "TecnoMarket"
 fecha = date.today()
 
-
 @app.route("/dashboard")
-def inicio():    
+def dashboard():    
 
     productos = [producto for producto in app.db.productos.find({})]
     clientes = [cliente for cliente in app.db.clientes.find({})]
-    pedidos = [pedido for pedido in app.db.pedidos.find({})]
-
+    pedidos = [
+        {"cliente": "Alejandro Rodriguez", "total": 3600.0, "fecha": "2025-05-01"},
+        {"cliente": "Pepe Sanchez", "total": 15.5, "fecha": "2025-05-03"},
+        {"cliente": "Manolo Gomez", "total": 5000.0, "fecha": "2025-05-04"}
+    ]
     total_stock = 0
     for producto in productos:
         total_stock += producto["stock"]
 
     clientes_activos = 0
     for cliente in clientes:
-        if cliente["activo"]:
+
+        activo = cliente["activo"]
+        activo = activo.lower()
+        if activo == "activo" :
             clientes_activos += 1
 
     cliente_pedido = clientes[0]
     for cliente in clientes:
-        if cliente["pedidos"] > cliente_pedido["pedidos"]:
+        if cliente["numero_pedidos"] > cliente_pedido["numero_pedidos"]:
             cliente_pedido = cliente
 
 
@@ -69,18 +78,18 @@ def añadir_producto():
     if request.method == "POST":
         productos = [app.db.productos.find()]
 
-        # Paso 1: Creamos una variable para guardar el id más alto
+
         id_mas_alto = 0
 
-        # Paso 2: Recorremos cada producto y revisamos su id
+
         for producto in productos:
             if "id" in producto and producto["id"] > id_mas_alto:
                 id_mas_alto = producto["id"]
 
-        # Paso 3: El nuevo id será uno más
+
         nuevo_id = id_mas_alto + 1
 
-        # Crear y guardar el producto
+
         nuevo_producto = {
             "id": nuevo_id,
             "nombre": request.form["nombre"],
@@ -92,44 +101,50 @@ def añadir_producto():
         app.db.productos.insert_one(nuevo_producto)
         mensaje = "Producto añadido correctamente."
 
-    return render_template("añadir_producto.html", mensaje=mensaje)
+    return render_template("añadir_producto.html", mensaje=mensaje, fecha=fecha)
 
 
-# Ruta: Ver productos
+
 @app.route("/productos")
 def ver_productos():
-    productos = [app.db.productos.find()]
-    return render_template("lista_productos.html", productos=productos)
+    productos_cursor = app.db.productos.find()
+    productos = []
+    for producto in productos_cursor:
+        producto["_id"] = str(producto["_id"])
+        productos.append(producto)
+    return render_template("lista_productos.html", productos=productos,fecha=fecha)
 
-# Ruta: Detalle producto
-@app.route("/productos/<int:id_producto>")
+
+@app.route("/productos/<id_producto>")
 def detalle_producto(id_producto):
-    producto = app.db.productos.find_one({"id": id_producto})
+    producto = app.db.productos.find_one({"_id": ObjectId(id_producto)})
     if producto:
-        return render_template("detalle_producto.html", producto=producto)
+        return render_template("detalle_producto.html", producto=producto,fecha=fecha)
     else:
-        return render_template("404.html"), 404
+        return render_template("404.html",fecha=fecha), 404
 
-# Ruta: Registro de usuario
-@app.route("/registro-usuario", methods=["GET", "POST"])
+
+@app.route("/registro-usuarios", methods=["GET", "POST"])
 def registro_usuario():
     mensaje = ""
     if request.method == "POST":
         nombre = request.form["nombre"]
         email = request.form["email"]
         contraseña = request.form["contraseña"]
-        nuevo_usuario = Usuario(nombre, email, contraseña, datetime.now())
+        activo =request.form["activo"]
+        numero_pedidos =int(request.form["pedidos"])
+        nuevo_usuario = Usuario(nombre, email, contraseña, datetime.now() ,activo ,numero_pedidos)
         app.db.clientes.insert_one(nuevo_usuario.to_dict())
         mensaje = "Usuario registrado con éxito."
-    return render_template("registro_usuario.html", mensaje=mensaje)
+    return render_template("registro_usuarios.html", mensaje=mensaje, fecha=fecha)
 
-# Ruta: Lista de usuarios
+
 @app.route("/usuarios")
 def lista_usuarios():
     usuarios = [app.db.clientes.find()]
-    return render_template("lista_usuarios.html", usuarios=usuarios)
+    return render_template("lista_usuarios.html", usuarios=usuarios, fecha=fecha)
 
-# Ruta: Error 404
+
 @app.errorhandler(404)
 def pagina_no_encontrada(e):
     return render_template("404.html"), 404
